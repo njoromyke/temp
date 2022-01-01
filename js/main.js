5975e7707ad4c0ced1bef079dec4ad1143dd9a43
ngahika01 <mykenjoroge01@gmail.com>
2022-01-01 14:23:09 +0300

    resume template

--- js/main.js
+++ js/main.js
@@ -1,8 +1,34 @@
-$(document).ready(function(){
-    if ($(window).width() < 990) {
-      $("#ham").click(function(){
-
-          $(".nav-all").toggle().css("visibility","visible")
-      })
-    }
-})
+$(document).ready(function () {
+  if ($(window).width() < 990) {
+    $("#ham").click(function () {
+      $(".nav-all").toggle().css("visibility", "visible");
+    });
+  }
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 2,
+        nav: false,
+        loop: true,
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: true,
+      },
+    },
+  });
+  // All animations will take exactly 500ms
+  var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 500,
+    speedAsDuration: true,
+  });
+});
